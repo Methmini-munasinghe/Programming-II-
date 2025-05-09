c17bd045de89b20c8eba9e578e6a5e7b8ddb2224
Geethanjana karunarathna <2022t01541@stu.cmb.ac.lk>
2025-05-09 20:05:03 +0530

    Update cipher.h

--- cipher.h
+++ cipher.h
@@ -1,1 +1,13 @@
+#ifndef CIPHER_H_INCLUDED
+#define CIPHER_H_INCLUDED
+
+#define ALPHABET 26
+
+// Function declarations using array instead of struct
+void initializeQueue(char letters[], int *front, int *rear);
+int getLetterIndex(char letters[], char letter);
+void encryptText(char letters[], const char *inputText, char *encryptedText, int shiftKey);
+void decryptText(char letters[], const char *encryptedText, char *decryptedText, int shiftKey);
+
+#endif // CIPHER_H_INCLUDED
 
