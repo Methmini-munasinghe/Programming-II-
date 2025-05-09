57ca2954d82a18614ad297d7c9d28cbe9773ae48
Thilina-Prasath <2022to1546@stu.cmb.ac.lk>
2025-05-10 00:22:16 +0530

    encryption and decryption

--- cipher.c
+++ cipher.c
@@ -36,3 +36,20 @@ void encryptText(char letters[], const char *inputText, char *encryptedText, int
     }
     encryptedText[strlen(inputText)]='\0';
 }
+
+// Caesar decryption
+void decryptText(char letters[], const char *encryptedText, char *decryptedText, int shiftKey) {
+    int length = ALPHABET;
+    for (int i = 0; encryptedText[i] != '\0'; i++) {
+        char currentChar = encryptedText[i];
+        if (isalpha(currentChar)) {
+            char upper = toupper(currentChar);
+            int index = getLetterIndex(letters, upper);
+            int originalIndex = (index - shiftKey + length) % length;
+            decryptedText[i] = isupper(currentChar) ? letters[originalIndex] : tolower(letters[originalIndex]);
+        } else {
+            decryptedText[i] = currentChar;
+        }
+    }
+    decryptedText[strlen(encryptedText)] = '\0';
+}
